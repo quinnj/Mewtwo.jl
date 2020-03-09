a6c6830b8d848efa148266a8b02866af621c6fc1
Jacob Quinn <quinn.jacobd@gmail.com>
2020-03-08 23:58:43 -0600

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -p 0.0.0.0:8081:8081/tcp -p 0.0.0.0:8082:8082/tcp quinnj/mewtwo > mewtwo.log 2>&1
+nohup docker run -p 0.0.0.0:8081:8081/tcp -p 0.0.0.0:8082:8082/tcp quinnj/mewtwo > mewtwo.log 2>&1 &
